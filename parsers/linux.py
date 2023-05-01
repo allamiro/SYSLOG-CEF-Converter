034ef6adedad1cd2760cdb45ff3c6fdc0fca5eac
Tamir Suliman <allamiro@gmail.com>
2023-05-01 23:42:12 +0200

    Update linux.py

--- parsers/linux.py
+++ parsers/linux.py
@@ -1,1 +1,8 @@
-
+def parse_linux_syslog(message):
+    match = re.match('(?P<timestamp>\w+\s+\d+\s+\d+:\d+:\d+)\s+(?P<hostname>\S+)\s+(?P<program>\S+)\[(?P<pid>\d+)\]:\s+(?P<message>.+)', message)
+    if match:
+        fields = match.groupdict()
+        cef_event = cef_header + f"Linux|{fields['hostname']}|{fields['program']}|1.0|{fields['message']}"
+        return cef_event
+    else:
+        return None
